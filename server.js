c7df2ced6a42789812f2c4d3af1872148a228e25
Jorge <jorgevillegas77@gmail.com>
2020-02-10 21:51:42 -0800

    Status File

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ import express from "express";
 const app = express();
 
 app.get("/");
+
+// Start our server...
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on port " + server.address().port);
+});

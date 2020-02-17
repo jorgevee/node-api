ba48ccefbe1d24ab117af0843edfe2212699163d
Jorge <jorgevillegas77@gmail.com>
2020-02-16 21:56:44 -0800

    Server.js Edit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-
-app.get("/");
-
+// Add middleware for parsing URL encoded bodies (which are usually sent by browser)
+app.use(cors());
 // Start our server...
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + server.address().port);
 });
+
+export default app;
